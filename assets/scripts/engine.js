c062fa7f6ecbd3bb3192c91eb76158db44579909
TPWLU <sppaulisto972@gmail.com>
2024-12-09 15:45:42 -0300

    Add Pokemon class to engine.js

---
+++ assets/scripts/engine.js
@@ -0,0 +1,39 @@
+class Pokemon {
+    nationalNumber;
+    pokeType;
+    pokeSpecies;
+    height;
+    weight;
+    abilities;
+    ev;
+    catchRate;
+    baseFriendship;
+    baseXP;
+    growthRate;
+    /*
+    Order of stats:
+    1. HP (hp)
+    2. Defense (def)
+    3. Special Attack (spAtk)
+    4. Special Defense (spDef)
+    5. Speed (speed)
+    */
+    stats;
+    level;
+
+    constructor(nationalNumber, pokeType, pokeSpecies, height, weight, abilities, ev, catchRate, friendship, xp, growthRate, stats, level) {
+        this.nationalNumber = nationalNumber;
+        this.pokeType = pokeType;
+        this.pokeSpecies = pokeSpecies;
+        this.height = height;
+        this.weight = weight;
+        this.abilities = abilities;
+        this.ev = ev;
+        this.catchRate = catchRate;
+        this.baseFriendship = friendship;
+        this.baseXP = xp;
+        this.growthRate = growthRate;
+        this.stats = stats;
+        this.level = level;
+    }
+}
