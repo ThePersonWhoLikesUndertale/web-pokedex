da7b07bfcc64c86251407f81ff340aef6138373a
TPWLU <sppaulisto972@gmail.com>
2024-12-13 15:24:39 -0300

    Update: convert pokeapi model to local model

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -1,19 +1,15 @@
-function convertPokemonTypesLi(pokemonTypes) {
-  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
-
 function convertPokemonToLI(pokemon) {
   return `
         <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ul class="types">
-                    ${convertPokemonTypesLi(pokemon.types).join("")}
+                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`)}
                 </ul>
 
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
     `;
