a9c48f2d335f1f6c8c7d7496203d28ce4b7d0ff1
TPWLU <sppaulisto972@gmail.com>
2024-12-13 16:41:22 -0300

    Update: add feature to load more pokemon

--- assets/scripts/engine.js
+++ assets/scripts/engine.js
@@ -1,29 +1,41 @@
-function convertPokemonToLI(pokemon) {
-  return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
 
-            <div class="detail">
-                <ul class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
-                </ul>
+function loadPokemonItens(offset, limit) {
+  pokeApi
+    .getPokemons(offset, limit)
+    .then((pokedex = []) => {
+      pokemonList.innerHTML += pokedex
+        .map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
 
-                <img src="${pokemon.photo}" alt="${pokemon.name}">
-            </div>
-        </li>
-    `;
+                <div class="detail">
+                    <ul class="types">
+                        ${pokemon.types
+                          .map((type) => `<li class="type ${type}">${type}</li>`)
+                          .join("")}
+                    </ul>
+
+                    <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+            </li>
+          `).join("");
+    })
+    .catch((error) => console.error(error));
 }
 
 function initialize() {
-  const pokemonList = document.getElementById("pokemonList");
+  const limit = 9;
+  let offset = 0;
 
-  pokeApi
-    .getPokemons()
-    .then((pokedex) => {
-      pokemonList.innerHTML = pokedex.map(convertPokemonToLI).join("");
-    })
-    .catch((error) => console.error(error));
+  loadPokemonItens(offset, limit);
+
+  loadMoreButton.addEventListener("click", () => {
+    offset += limit;
+    loadPokemonItens(offset, limit);
+  });
 }
 
 initialize();
